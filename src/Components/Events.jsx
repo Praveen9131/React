55bb91e28abcd2b7441b982a394af39364be33dd
Shaibaaz077 <mohdshaibaaz077@gmail.com>
2025-08-29 10:28:22 +0530

    29

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -163,7 +163,7 @@ export default function Events() {
               <li className="flex items-start gap-3">
                 <span className="mt-1 h-2 w-2 rounded-full bg-[--primary]" />
                 <span>
-                  <span className="font-semibold">Last Date:</span> 5 Sep 2025
+                  <span className="font-semibold">Last Date:</span> 15 Sep 2025
                 </span>
               </li>
             </ul>
